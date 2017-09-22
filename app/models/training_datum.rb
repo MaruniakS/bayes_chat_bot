6b7714e57903b0251aa718b22855b749f72370b3
Stanislav Maruniak <stanislav.maruniak@kindgeek.com>
2017-09-22 20:18:00 +0300

    validation

--- app/models/training_datum.rb
+++ app/models/training_datum.rb
@@ -1,3 +1,8 @@
 class TrainingDatum < ApplicationRecord
   belongs_to :category
+
+  validates :sentence, presence: true, uniqueness: true,
+            format: { with: /\A[a-zA-z ]+\z/, message: 'Only letters are allowed' }
+
+  scope :get_by_type, ->(category_type) { where category: category_type}
 end
